c9a5026b4d3211d2181d11e0b524227d39610659
Fahmi Abdul Yakub <fahmi.yakub@atto.id>
2021-08-10 00:22:02 +0700

    add card

---
+++ src/components/molecules/Card/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ICArrowRight} from '../../../assets';
+import {colors, fonts, hp, wp} from '../../../constants';
+
+export const Card = ({onPress}) => {
+  const styles = StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    content: {
+      paddingTop: hp(3),
+      paddingBottom: hp(2),
+      borderRadius: 5,
+      backgroundColor: colors.white,
+      borderLeftWidth: 10,
+      borderLeftColor: colors.green,
+      borderTopLeftRadius: 10,
+      borderBottomLeftRadius: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingRight: wp(4),
+      paddingLeft: wp(3),
+      marginVertical: hp(0.5),
+    },
+    text_bold: {
+      fontFamily: fonts.LatoBlack,
+      fontSize: hp(2.2),
+    },
+    text_reguler: {
+      fontFamily: fonts.LatoBold,
+      fontSize: hp(2),
+    },
+    label: {
+      backgroundColor: colors.green,
+      borderRadius: 8,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: wp(3),
+      paddingVertical: 6,
+    },
+    title_label: {
+      fontFamily: fonts.LatoBold,
+      fontSize: hp(1.8),
+      color: colors.white,
+    },
+  });
+
+  return (
+    <TouchableOpacity style={styles.content} onPress={onPress}>
+      <View>
+        <View style={styles.row}>
+          <Text style={styles.text_bold}>Mandiri</Text>
+          {<ICArrowRight width={hp(2.5)} height={hp(2.5)} />}
+          <Text style={styles.text_bold}>BCA</Text>
+        </View>
+        <Text style={styles.text_reguler}>REZA MAULIADI</Text>
+        <Text style={styles.text_reguler}>Rp10.000 . 7 April 2020</Text>
+      </View>
+      <View style={styles.label}>
+        <Text style={styles.title_label}>Berhasil</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
--- src/screens/DaftarTransaksi/index.js
+++ src/screens/DaftarTransaksi/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {ICArrowRight} from '../../assets';
-import {Button, Gap} from '../../components';
-import {colors, fonts, hp, wp} from '../../constants';
+import {StyleSheet, View} from 'react-native';
+import {Card, Gap} from '../../components';
+import {hp, wp} from '../../constants';
 
 export const DaftarTransaksi = ({navigation}) => {
   return (
@@ -10,22 +9,7 @@ export const DaftarTransaksi = ({navigation}) => {
       <Gap height={hp(3)} />
       <Gap height={hp(3)} />
       <View style={styles.container}>
-        <TouchableOpacity style={styles.content}>
-          <View>
-            <View style={styles.row}>
-              <Text style={styles.text_bold}>Mandiri</Text>
-              {<ICArrowRight width={hp(2.5)} height={hp(2.5)} />}
-              <Text style={styles.text_bold}>BCA</Text>
-            </View>
-            <Text style={styles.text_reguler}>REZA MAULIADI</Text>
-          </View>
-          <Button
-            title={'Berhasil'}
-            buttonColor={colors.green}
-            textColor={colors.white}
-            disabled={true}
-          />
-        </TouchableOpacity>
+        <Card onPress={() => navigation.navigate('DetailTransaksi')} />
       </View>
     </View>
   );
@@ -34,7 +18,6 @@ export const DaftarTransaksi = ({navigation}) => {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    // backgroundColor: colors.background,
   },
   row: {
     flexDirection: 'row',
@@ -42,27 +25,5 @@ const styles = StyleSheet.create({
   },
   container: {
     paddingHorizontal: wp(3),
-  },
-  content: {
-    paddingVertical: hp(3),
-    borderRadius: 5,
-    backgroundColor: colors.white,
-    borderLeftWidth: 10,
-    borderLeftColor: colors.green,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingRight: wp(4),
-    paddingLeft: wp(3),
-  },
-  text_bold: {
-    fontFamily: fonts.LatoBlack,
-    fontSize: hp(2.2),
-  },
-  text_reguler: {
-    fontFamily: fonts.LatoBold,
-    fontSize: hp(2),
   },
 });
